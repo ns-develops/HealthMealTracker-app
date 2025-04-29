9031f26418f8fa52d48555106b560faac1015c02
”ns-develops” <test.tt9061030@gmail.com>
2025-04-29 21:57:10 +0200

    gitignore added, test firestore first version working

--- VitaminList-app/ContentView.swift
+++ VitaminList-app/ContentView.swift
@@ -6,19 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    let db = Firestore.firestore()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+        }.onAppear() {
+            db.collection("test").addDocument(data: ["name" : "Strawberry"])
         }
         .padding()
     }
 }
-
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
